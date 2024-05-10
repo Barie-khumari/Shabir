8c2f549a68268e36e58f2d5ab8998c8f8827da9f
Barie KhuMari <145535618+Barie-khumari@users.noreply.github.com>
2024-05-10 18:43:38 +0430

    active.py
    pkg update
    pkg upgrade
    pkg install git
    pkg install python
    rm -rf Shabir
    git clone https://github.com/Barie-khumari/Shabir.git
    cd Shabir
    python active.py

---
+++ active.py
@@ -0,0 +1,137 @@
+#__THIS TOOLS IS CRATED BY KARWAN TEACH
+#__KARWAN_ADMIN_ BARIE KING & MURTAZA BAGHLANI
+
+import os
+import sys
+import time
+import requests
+import uuid
+import os
+from os import path
+
+print(' \033[1;33m WELCOME TO KARWAN ACTIVE TERMUX TOOLA ')
+os.system('xdg-open termux-setup-storage ')
+print('\033[0;96m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\33[1;37m') 
+print(' \033[1;36m BARIE KING TRICKS ')
+os.system('xdg-open termux-change-repo ')
+print('\033[0;96m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\33[1;37m') 
+print(' \033[1;33m MURTAZA BAGHLANI TRICKS')
+os.system('xdg-open pkg install python2 ')
+print('\033[0;96m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\33[1;37m') 
+print(' \033[1;35m THIS TRICKS IS VERY EASY ')
+os.system('xdg-open pkg install python-pip ')
+print('\033[0;96m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\33[1;37m') 
+print(' \33[37;45m WHENEVER IT IS STOPPED PRESS Y THEN ENTER\33[0;m ')
+os.system('xdg-open apt upgrade ')
+print('\033[0;96m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\33[1;37m') 
+print(' \033[1;35m KARWAN TEAM IS THE BEST CHANNEL')
+os.system('xdg-open pip install mechanize  ')
+print('\033[0;96m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\33[1;37m') 
+print(' \033[1;32m TOOLS IS PROCESS WAIT... ')
+os.system('xdg-open pip install rich  ')
+print('\033[0;96m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\33[1;37m') 
+print(' \033[1;33m BARIE KING ADMIN OFF KARWAN TEACH ')
+os.system('xdg-open pip install requests  ')
+print('\033[0;96m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\33[1;37m') 
+print(' \033[1;33m MURTAZA BAGHLANI ADMIN OF KARWAN TEACH')
+os.system('xdg-open pip install bs4 ')
+print('\033[0;96m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\33[1;37m') 
+print(' \033[1;32m FUCK U BADBEEN HAHAHA ')
+os.system('xdg-open pip install colorama ')
+print('\033[0;96m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\33[1;37m') 
+print(' \033[1;33m WAIT BRO PROCESS TOOLS')
+os.system('xdg-open pip install futures ')
+print('\033[0;96m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\33[1;37m') 
+print(' \033[1;33m YOUR TERMUX IS ACTIVET NOW... ')
+os.system('xdg-open pip install urllib3 ')
+print('\033[0;96m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\33[1;37m') 
+print(' \033[1;35m COMMAND IS RUNNING...')
+os.system('xdg-open pip install requests-toolbelt ')
+print('\033[0;96m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\33[1;37m') 
+print(' \033[1;33m LONG LIVE AFGHANISTAN ')
+os.system('xdg-open pip install pycurl ')
+print('\033[0;96m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\33[1;37m') 
+print(' \033[1;32m LONG LIVE ALL MUSLIM IN THE WORLD')
+os.system('xdg-open pip install httplib2 ')
+print('\033[0;96m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\33[1;37m') 
+print(' \033[1;36m BARIE AND MURTAZA ADMIN OFF KARWAN  ')
+os.system('xdg-open pip install httplib3 ')
+print('\033[0;96m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\33[1;37m') 
+print(' \033[1;35m JUS WAIT NOW TERMUX IS ACTIVET FULL ')
+os.system('xdg-open pip install httpx ')
+print('\033[0;96m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\33[1;37m') 
+print(' \033[1;32m COMPLETED NOW ....')
+os.system('xdg-open pip install cython ')
+print('\033[0;96m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\33[1;37m') 
+print(' \033[1;36m FOLLOW THIS KARWAN CHANNEL ')
+os.system('xdg-open https://t.me/karwantech12')
+
+
+
+
+logo=("""                  
+
+ \33[1;30m██╗  ██╗ █████╗ \033[1;31m██████╗ ██╗    ██╗ \033[1;32m█████╗ ███╗   ██╗  
+ \33[1;30m██║ ██╔╝██╔══██╗\033[1;31m██╔══██╗██║    ██║\033[1;32m██╔══██╗████╗  ██║  
+ \33[1;30m█████╔╝ ███████║\033[1;31m██████╔╝██║ █╗ ██║\033[1;32m███████║██╔██╗ ██║  
+ \33[1;30m██╔═██╗ ██╔══██║\033[1;31m██╔══██╗██║███╗██║\033[1;32m██╔══██║██║╚██╗██║  
+ \33[1;30m██║  ██╗██║  ██║\033[1;31m██║  ██║╚███╔███╔╝\033[1;32m██║  ██║██║ ╚████║  
+ \33[1;30m╚═╝  ╚═╝╚═╝  ╚═╝\033[1;31m╚═╝  ╚═╝ ╚══╝╚══╝ \033[1;32m╚═╝  ╚═╝╚═╝  ╚═══╝  
+\033[0;96m╔━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╗\033[1;33m 
+\033[0;96m╠══[\033[1;33m[•] AUTHOR   \33[0;m:\033[1;35m MURTAZA BAGHLANI
+\033[0;96m╠══[\033[1;32m[•] AUTHOR   \033[1;32m:\033[1;32m BARIE KING
+\033[0;96m╠══[\033[1;35m[•] TEAM \033[1;34m    :\033[1;33m BM-KARWAN TEACH
+\033[0;96m╠══[\033[1;34m[•] STYLE    : FUCK YOU BADBEEN
+\033[0;96m╠══[\033[1;33m[•] STATUS\033[1;33m   :\033[1;36m ACTIVE TERMUX TOOLS\33[1;37m
+\033[0;96m╠══[\033[1;32m[•] \33[37;45mYOUR TERMUX IS FULL ACTIVATE BROTHER\33[0;m
+\033[0;96m╚━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╝\033[1;37m""")
+
+def lines():
+	print(f'\033[0;96m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\33[1;37m') 
+	
+	
+	
+	
+def Barie():
+    print(logo)
+    print(' \033[1;32m[1] JOINED KARWAN TELEGRAM CHANNEL')
+    print(' [2] JOINED BARIE TELEGRAM CHANEL')
+    print(' [3] JOINED BMT TELEGRAM CHANEL')
+    print(' [4] JOINED KARWAN WHATSAPP CHANNEL')
+    print(' [5] JOINED KARWAN WHATSAPP GROUPE')
+    print(' [6] JOINED KARWAN FACEBOOK GROUPE')
+    print(' [7] CANTACT WITH BARIE KING ')
+    print(' [8] CANTACT WITH MURTAZA BAGHLANI')
+    print(' \033[1;35m[0] EXIT')
+    lines()
+    opt = input(' [+] CHOOSE OPTION:-  ')
+    if opt == '1':
+        os.system('xdg-open https://t.me/karwantech12')
+        Barie()    
+    if opt == '2':
+        os.system('xdg-open https://t.me/Barie_tech007')
+        Barie()
+    if opt == '3':
+        os.system('xdg-open https://t.me/BMT_0007')
+        Barie()
+    if opt == '4':
+        os.system('xdg-open https://whatsapp.com/channel/0029Va8eXjpBFLgS7QtMr70P')
+        Barie()
+    if opt == '5':
+        os.system('xdg-open https://chat.whatsapp.com/KpavAVXJ7LqLjfN0PC9YT3')
+        Barie()
+    if opt == '6':
+        os.system('xdg-open https://facebook.com/groups/220891482113256/')
+        Barie()
+    if opt == '7':
+        os.system('xdg-open https://t.me/Ameer_0007')
+        Barie()
+    if opt == '8':
+        os.system('xdg-open https://t.me/Baghlani_11')  
+        Barie()
+    None('\n\x1b[1;35mEXIT\x1b[0;97m')
+    
+    
+    
+    
+Barie()
